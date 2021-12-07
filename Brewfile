83376584701bd838bd5461817c828dad3a9a4bac
Sam Morrow <sammorrowdrums@github.com>
2021-12-07 16:14:54 +0100

    Initial dotfiles

---
+++ Brewfile
@@ -0,0 +1,39 @@
+brew "direnv"
+brew "rbenv"
+brew "grc"
+brew "hub"
+brew "gh"
+brew "go"
+brew "node"
+brew "graphviz" # for dot & tree-sitter grammar development
+brew "jq"
+brew "tree"
+brew "git"
+brew "zsh"
+brew "coreutils"
+brew "md5sha1sum"
+brew "openssh"
+brew "python3"
+brew "ipython"
+brew "azure-cli"
+
+# Development tools
+cask "github"
+cask "visual-studio-code"
+cask "sublime-text"
+
+# General apps
+cask "1Password"
+# cask "arq" # note: need arq 5 for now
+brew "gmailctl"
+# cask "divvy"
+cask "google-chrome"
+cask "slack"
+cask "spotify"
+cask "zoom"
+cask "notunes" #stop the rage-inducing opening of Apple Music by my bluetooth headphones
+# Extras (non-essential)
+# cask "graphiql"
+# cask "balenaetcher"
+# cask "arduino"
+# cask "steam"
---
+++ zsh/prompt.zsh
@@ -0,0 +1,85 @@
+autoload colors && colors
+# cheers, @ehrenmurdick
+# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+
+git_branch() {
+  echo $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+}
+
+git_dirty() {
+  # if [[ $st == "" ]]
+  st=$(git status 2>/dev/null | tail -n 1)
+  if [[ $st =~ 'fatal: Not a git repository' ]]
+  then
+    echo ""
+  else
+    if [[ $st =~ 'nothing to commit' ]]
+    then
+      echo "on %{$fg[green]%}$(git_prompt_info)%{$reset_color%}"
+    else
+      echo "on %{$fg[red]%}$(git_prompt_info)%{$reset_color%}"
+    fi
+  fi
+}
+
+git_prompt_info () {
+  ref=$(git symbolic-ref HEAD 2>/dev/null) || return
+  echo "${ref#refs/heads/}"
+}
+
+project_name () {
+  name=$(pwd | awk -F'GitHub/' '{print $2}' | awk -F/ '{print $1}')
+  echo $name
+}
+
+project_name_color () {
+  echo "%{\e[0;35m%}${name}%{\e[0m%}"
+}
+
+unpushed () {
+  git cherry -v origin/$(git_branch) 2>/dev/null
+}
+
+need_push () {
+  if [[ $(unpushed) == "" ]]
+  then
+    echo ""
+  else
+    echo " with %{$fg[magenta]%}unpushed%{$reset_color%}"
+  fi
+}
+
+ruby_prompt(){
+  rv=$(rbenv version-name)
+  if (echo $rv &> /dev/null)
+  then
+    echo "%{$fg[yellow]%}ruby $rv%{$reset_color%}"
+  elif $(which rvm &> /dev/null)
+  then
+    echo "%{$fg[yellow]%}$(rvm tools identifier)%{$reset_color%}"
+  else
+    echo ""
+  fi
+}
+
+function check_last_exit_code() {
+  local LAST_EXIT_CODE=$?
+  if [[ $LAST_EXIT_CODE -ne 0 ]]; then
+    local EXIT_CODE_PROMPT=' '
+    EXIT_CODE_PROMPT+="%{$fg[red]%}-%{$reset_color%}"
+    EXIT_CODE_PROMPT+="%{$fg_bold[red]%}$LAST_EXIT_CODE%{$reset_color%}"
+    EXIT_CODE_PROMPT+="%{$fg[red]%}-%{$reset_color%}"
+    echo "$EXIT_CODE_PROMPT"
+  fi
+}
+
+directory_name(){
+  echo "%{$fg[cyan]%}%1/%\/%{$reset_color%}"
+}
+
+date_time='%D{%m.%d.%Y} %@'
+
+export PROMPT=$'⑁ $(directory_name) $(project_name_color)$(git_dirty)$(need_push) ❯ '
+# export PROMPT=$'$(ruby_prompt) ⑁ $(directory_name) $(project_name_color)$(git_dirty)$(need_push) ❯ '
+# export RPROMPT=""
+RPROMPT='$(check_last_exit_code)'
